20dfe0368ab02d8eb0df8d563ac3cf61fc65d85f
michele <capimichi@gmail.com>
2019-02-27 09:48:46 +0100

    Comuni.json

--- app/code/local/Capimichi/CalcoloCodiceFiscale/Block/Calculator.php
+++ app/code/local/Capimichi/CalcoloCodiceFiscale/Block/Calculator.php
@@ -5,7 +5,9 @@ class Capimichi_CalcoloCodiceFiscale_Block_Calculator extends Mage_Core_Block_Te
     
     public function getCommons()
     {
-    
+        $url = "https://raw.githubusercontent.com/capimichi/m1_calcolocodicefiscale/master/comuni.csv";
+        
+        
     }
     
 }
