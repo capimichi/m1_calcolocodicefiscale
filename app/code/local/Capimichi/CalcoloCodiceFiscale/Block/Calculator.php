74d39955d4c42fa2e289f837f8dc05f7f27e1b43
michele <capimichi@gmail.com>
2019-02-27 09:52:02 +0100

    Fix

--- app/code/local/Capimichi/CalcoloCodiceFiscale/Block/Calculator.php
+++ app/code/local/Capimichi/CalcoloCodiceFiscale/Block/Calculator.php
@@ -2,12 +2,19 @@
 
 class Capimichi_CalcoloCodiceFiscale_Block_Calculator extends Mage_Core_Block_Template
 {
+    const COMUNI_JSON_ENDPOINT = "https://raw.githubusercontent.com/capimichi/m1_calcolocodicefiscale/master/comuni.json";
     
     public function getCommons()
     {
-        $url = "https://raw.githubusercontent.com/capimichi/m1_calcolocodicefiscale/master/comuni.csv";
-        
-        
+        $url = self::COMUNI_JSON_ENDPOINT;
+        $data = file_get_contents($url);
+        if ($data) {
+            $json = json_decode($data, true);
+            if (isset($json['comuni'])) {
+                return $json['comuni'];
+            }
+        }
+        return [];
     }
     
 }
